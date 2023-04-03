3c750bb22168befc466cb5cd17ee8f547e364cdd
EruditeCode <erudite.home@gmail.com>
2023-04-03 11:42:56 +0100

    Implemented basic x and y axis rotations.

--- class_Atom.py
+++ class_Atom.py
@@ -3,7 +3,5 @@ import numpy as np
 class Atom:
 	def __init__(self, element, x, y, z, bonds):
 		self.element = element
-		# Can manipulate this vector to render the image in different dimensions.
 		self.vector = np.matrix([x, y, z])
-		print(self.vector)
 		self.bonds = {bond[0]:bond[1] for bond in bonds}
--- main.py
+++ main.py
@@ -21,14 +21,36 @@ def main():
 	for atom in benzene:
 		molecule.append(Atom(atom[0], atom[1]*MAG, atom[2]*MAG, atom[3]*MAG, atom[4]))
 
+	rotate = False
+	rotate_start = None
 	while True:
 		for event in pg.event.get():
 			if event.type == pg.QUIT:
 				pg.quit()
 				exit()
+			if event.type == pg.MOUSEBUTTONDOWN:
+				if event.button == 1 and not rotate_start:
+					rotate_start = pg.mouse.get_pos()
 			if event.type == pg.MOUSEBUTTONUP:
-				for atom in molecule:
-					atom.vector = rotate_y(15, atom.vector)
+				if event.button == 1 and rotate_start:
+					rotate = True
+
+		# Update molecule position.
+		if rotate:
+			rotate_end = pg.mouse.get_pos()
+			dx = rotate_start[0]-rotate_end[0]
+			dy = rotate_start[1]-rotate_end[1]
+			angle = math.pi * (dy / HEIGHT)
+			for atom in molecule:
+				atom.vector = rotate_x(angle, atom.vector)
+			angle = math.pi * (dx / WIDTH)
+			for atom in molecule:
+				atom.vector = rotate_y(angle, atom.vector)
+
+			# Reset flags...
+			rotate = False
+			rotate_start = None
+
 
 		# Displaying the background surface.
 		screen.blit(bg, (0, 0))
@@ -46,19 +68,19 @@ def main():
 		clock.tick(60)
 
 
-def rotate_x(angle):
-	pass
-
 def rotate_y(angle, vector):
-	angle = math.radians(angle)
 	rotate_by = np.matrix([[math.cos(angle), 0, math.sin(angle)],
 				[0, 1, 0], 
 				[-math.sin(angle), 0, math.cos(angle)]])
 	result = np.matmul(vector, rotate_by)
 	return result
 
-def rotate_z(angle):
-	pass
+def rotate_x(angle, vector):
+	rotate_by = np.matrix([[1, 0, 0],
+				[0, math.cos(angle), -math.sin(angle)], 
+				[0, math.sin(angle), math.cos(angle)]])
+	result = np.matmul(vector, rotate_by)
+	return result
 
 
 if __name__ == "__main__":
