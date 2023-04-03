dc6f7eca18d270e2e8dfdfc1b4cc21cc9f94b978
EruditeCode <erudite.home@gmail.com>
2023-04-03 10:43:23 +0100

    Testing a basic rotation around the y-axis.

--- class_Atom.py
+++ class_Atom.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 class Atom:
 	def __init__(self, element, x, y, z, bonds):
 		self.element = element
-		self.x = x
-		self.y = y
-		self.z = z
+		# Can manipulate this vector to render the image in different dimensions.
+		self.vector = np.matrix([x, y, z])
+		print(self.vector)
 		self.bonds = {bond[0]:bond[1] for bond in bonds}
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame as pg
+import math
+import numpy as np
 from molecules import benzene, methane
 from class_Atom import Atom
 
@@ -24,6 +26,9 @@ def main():
 			if event.type == pg.QUIT:
 				pg.quit()
 				exit()
+			if event.type == pg.MOUSEBUTTONUP:
+				for atom in molecule:
+					atom.vector = rotate_y(15, atom.vector)
 
 		# Displaying the background surface.
 		screen.blit(bg, (0, 0))
@@ -32,13 +37,28 @@ def main():
 		# Drawing bonds.
 		for atom in molecule:
 			for key in atom.bonds.keys():
-				pg.draw.line(screen, (255,255,255), (atom.x+WIDTH//2, atom.y+HEIGHT//2), (molecule[key-1].x+WIDTH//2, molecule[key-1].y+HEIGHT//2), 5)
+				pg.draw.line(screen, (255,255,255), (atom.vector[0].item(0)+WIDTH//2, atom.vector[0].item(1)+HEIGHT//2), (molecule[key-1].vector[0].item(0)+WIDTH//2, molecule[key-1].vector[0].item(1)+HEIGHT//2), 5)
 		# Drawing atoms.
 		for atom in molecule:
-			pg.draw.circle(screen, (255,255,255), (atom.x+WIDTH//2,atom.y+HEIGHT//2), 10)
+			pg.draw.circle(screen, (255,255,255), (atom.vector[0].item(0)+WIDTH//2,atom.vector[0].item(1)+HEIGHT//2), 10)
 		
 		pg.display.update()
 		clock.tick(60)
+
+
+def rotate_x(angle):
+	pass
+
+def rotate_y(angle, vector):
+	angle = math.radians(angle)
+	rotate_by = np.matrix([[math.cos(angle), 0, math.sin(angle)],
+				[0, 1, 0], 
+				[-math.sin(angle), 0, math.cos(angle)]])
+	result = np.matmul(vector, rotate_by)
+	return result
+
+def rotate_z(angle):
+	pass
 
 
 if __name__ == "__main__":
