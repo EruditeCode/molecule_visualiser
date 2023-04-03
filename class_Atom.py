589ad0086e45fd40b6ebfe3d965b3ecf8871135f
EruditeCode <erudite.home@gmail.com>
2023-04-03 16:59:16 +0100

    Improved the depth effect and added color for different elements.

--- class_Atom.py
+++ class_Atom.py
@@ -6,3 +6,9 @@ class Atom:
 		self.fixed_vector = np.matrix([x, y, z])
 		self.vector = np.matrix([x, y, z])
 		self.bonds = {bond[0]:bond[1] for bond in bonds}
+
+	def link_bonds_to_atoms(self, atoms):
+		temp_bonds = []
+		for key in self.bonds.keys():
+			temp_bonds.append((atoms[key-1], self.bonds[key]))
+		self.bonds = temp_bonds
--- main.py
+++ main.py
@@ -12,12 +12,15 @@ def main():
 	clock = pg.time.Clock()
 
 	bg = pg.Surface((WIDTH, HEIGHT))
-	bg.fill((20,20,20))
+	bg.fill((130,150,150))
 
 	# Load molecule using atom class.
 	molecule = []
 	for atom in methane:
 		molecule.append(Atom(atom[0], atom[1], atom[2], atom[3], atom[4]))
+
+	for atom in molecule:
+		atom.link_bonds_to_atoms(molecule)
 
 	MAG = 100
 	r_start = False
@@ -34,6 +37,9 @@ def main():
 			if event.type == pg.MOUSEBUTTONUP:
 				if event.button == 1 and r_start:
 					r_end = True
+				if event.button == 3:
+					centre = find_new_centre(pg.mouse.get_pos(), molecule, HEIGHT, WIDTH, MAG)
+					centre_on_new(centre, molecule)
 			if event.type == pg.MOUSEWHEEL:
 				MAG += event.y
 
@@ -59,6 +65,7 @@ def main():
 
 
 		# Prior to displaying, order atoms in molecule by z-position.
+		molecule.sort(key=lambda x: x.vector[0].item(2), reverse=False)
 
 		# Displaying the background surface.
 		screen.blit(bg, (0, 0))
@@ -66,18 +73,22 @@ def main():
 		# Drawing the molecule.
 		# Drawing bonds.
 		for atom in molecule:
-			for key in atom.bonds.keys():
-				x1 = atom.vector[0].item(0)*MAG+WIDTH//2
-				y1 = atom.vector[0].item(1)*MAG+HEIGHT//2
-				x2 = molecule[key-1].vector[0].item(0)*MAG+WIDTH//2
-				y2 = molecule[key-1].vector[0].item(1)*MAG+HEIGHT//2
+			for bond in atom.bonds:
+				x1 = atom.vector[0,0]*MAG+WIDTH//2
+				y1 = atom.vector[0,1]*MAG+HEIGHT//2
+				x2 = bond[0].vector[0,0]*MAG+WIDTH//2
+				y2 = bond[0].vector[0,1]*MAG+HEIGHT//2
 				pg.draw.line(screen, (255,255,255), (x1, y1), (x2, y2), 5)
 		# Drawing atoms.
 		for atom in molecule:
-			x = atom.vector[0].item(0)*MAG+WIDTH//2
-			y = atom.vector[0].item(1)*MAG+HEIGHT//2
-			radius = MAG//5+atom.vector[0].item(2)*MAG//20
-			pg.draw.circle(screen, (255,255,255), (x, y), radius) #12
+			x = atom.vector[0,0]*MAG+WIDTH//2
+			y = atom.vector[0,1]*MAG+HEIGHT//2
+			if atom.element == "H":
+				radius = (covalent_radii[atom.element]*MAG*0.4) + (atom.vector[0].item(2))
+			else:
+				radius = (covalent_radii[atom.element]*MAG*0.3) + (atom.vector[0].item(2))
+			#radius = MAG//5+atom.vector[0].item(2)*MAG//20
+			pg.draw.circle(screen, atom_color[atom.element], (x, y), radius) #12
 		
 		pg.display.update()
 		clock.tick(60)
@@ -96,6 +107,22 @@ def rotate_x(angle, vector):
 				[0, math.sin(angle), math.cos(angle)]])
 	result = np.matmul(vector, rotate_by)
 	return result
+
+def find_new_centre(position, molecule, HEIGHT, WIDTH, MAG):
+	position = ((position[0]-WIDTH//2)/MAG, (position[1]-HEIGHT//2)/MAG)
+	min_dist, min_atom = 10000, None
+	for atom in molecule:
+		coord = (atom.vector[0].item(0),atom.vector[0].item(1))
+		if math.dist(position, coord) < min_dist:
+			min_dist = math.dist(position, (atom.vector[0].item(0),atom.vector[0].item(1)))
+			min_atom = atom
+	return min_atom
+
+def centre_on_new(centre_atom, molecule):
+	sub_matrix = centre_atom.vector.copy()
+	for atom in molecule:
+		atom.vector = atom.vector - sub_matrix
+		atom.fixed_vector = atom.vector.copy()
 
 
 if __name__ == "__main__":
